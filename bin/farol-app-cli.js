2359dbbce518d61b07b9e14f11330fb5de099d79
Felipe Plets <felipe.plets@menvia.com>
2019-03-07 22:13:49 -0300

    Add some checks and enhancements

--- bin/farol-app-cli.js
+++ bin/farol-app-cli.js
@@ -4,6 +4,21 @@
 const args = process.argv.splice(process.execArgv.length + 2);
 
 const myLibrary = require('../lib/index.js');
+const availableCommands = [
+  'build',
+  'checkout',
+  'create',
+  'deploy',
+  'promote',
+  'publish',
+  'setup',
+  'store'
+];
 
-// Displays the text in the console
-myLibrary.run(args);
+if (args[0] === 'init') {
+  myLibrary.init();
+} else if (availableCommands.includes(args[0])) {
+  myLibrary.run(args);
+} else {
+  myLibrary.notSupported();
+}
